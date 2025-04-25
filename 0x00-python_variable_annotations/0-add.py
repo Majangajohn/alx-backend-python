deea7ab494d6eee972af9a863d22e22cae82d1fb
Majangajohn <47883618+Majangajohn@users.noreply.github.com>
2025-04-25 11:30:39 +0300

    Update 0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -3,11 +3,9 @@
 takes a float a and a float b as arguments
 and returns their sum as a float."""
 
-
 def add(a:float, b:float)-> float:
 	"""_summary_
-
-	Args:
+ 	Args:
 		a (float): _description_
 		b (float): _description_
 
